e779033c8d88d6b9784f588631903dad55145aee
nmondon <nmondon@lefigaro.fr>
2015-02-13 18:34:05 +0100

    - Genetic algorithm : Unit has now a 2d context

--- web_modules/genetic/unit.js
+++ web_modules/genetic/unit.js
@@ -7,11 +7,20 @@ var Unit = {
 
     vecs: [],
 
-    init: function(numVecs, weight){
-        this.vecs = _.range(0, numVecs).map(function(){
+    context: null,
+
+    init: function (numVecs, weight, w, h) {
+        // http://stackoverflow.com/questions/3892010/create-2d-context-without-canvas
+        this.context = window.document.createElement('canvas').getContext('2d');
+        this.context.canvas.width = w;
+        this.context.canvas.height = h;
+        this.vecs = _.range(0, numVecs).map(function () {
             return vectorUtil.generate(weight);
         });
         return this;
+    },
+    retrieveMatrix: function () {
+        return this.context.getImageData(0, 0, this.context.canvas.width, this.context.canvas.height);
     }
 
 };
